51d8854627d80daee598b150d3be4bd12694ed16
Slawa <depidsvy@nintendo.de>
2022-06-23 12:48:59 +0200

    detect seaPixels

---
+++ pixel.js
@@ -0,0 +1,76 @@
+const img = new Image();
+img.crossOrigin = 'anonymous';
+img.src = 'img.png';
+const canvas = document.getElementById('canvas');
+console.log(canvas);
+let ctx
+
+img.onload = function () {
+	img.style.display = 'none';
+	console.log(img.width, img.height);
+	canvas.style.width = img.width + 'px';
+	canvas.style.height = img.height + 'px'
+	canvas.width = img.width
+	canvas.height = img.height
+	ctx = canvas.getContext('2d');
+	console.log(ctx);
+	resetCanvas();
+}
+
+function resetCanvas() {
+	ctx.drawImage(img, 0, 0);
+}
+
+function rgba(data) {
+	return `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${(data[3] ?? 255) / 255})`
+}
+
+function pick(event, destination) {
+	const x = event.layerX;
+	const y = event.layerY;
+	const pixel = ctx.getImageData(x, y, 1, 1);
+	const data = pixel.data;
+	// console.log({data})
+
+	const pickColor = rgba(data);
+	ctx.rect(x, y, 5, 5)
+
+	return {x, y, pickColor};
+}
+
+let seaColor = [157, 192, 245] // 'rgba(157, 192, 245, 1)'
+
+function getSeaPercent(x, y, radius) {
+	let seaPixels = 0
+	for (let yy = y - radius; yy < y + radius; yy++) {
+		for (let xx = x - radius; xx < x + radius; xx++) {
+			let {data} = ctx.getImageData(xx, yy, 1, 1);
+			data = data.slice(0, 3)
+			if (data.join(',') === seaColor.join(',')) {
+				seaPixels++
+			}
+		}
+	}
+	let denom = radius * radius * 2 * 2;
+	console.log(seaPixels, '/', denom, seaPixels / denom)
+	return seaPixels / denom
+}
+
+
+canvas.addEventListener('click', function (event) {
+	let {x, y, pickColor} = pick(event);
+	console.log(x, y, pickColor);
+	resetCanvas();
+
+	let radius = 50
+	const seaPercent = getSeaPercent(x, y, radius);
+
+	ctx.beginPath();
+	ctx.arc(x, y, radius, 0, 2 * Math.PI);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.fillStyle = rgba(seaColor.map(x => x * seaPercent));
+	ctx.arc(x, y, radius - 5, 0, 2 * Math.PI);
+	ctx.fill();
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+https://www.google.com/maps/@38.1716561,-0.7766818,9z/data=!5m1!1e4
+
+img.png
+
+top-left: 39.287922, -3.914652
+top-right: 39.313427, 2.237692
+
+bottom-left: 37.136623, -3.933878
+bottom-right: 37.029258, 2.237692
